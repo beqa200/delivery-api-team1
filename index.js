31d93cc3d75cd133df6b4f318cd838a8e30913b5
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-12 11:59:41 +0400

    feat: create orders route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "welcome to postgress" });
 });
+app.use("/api/orders", orderRoutes);
 
 app.listen(port, () => {
   console.log("server is running on", port);
