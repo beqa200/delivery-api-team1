5bec74d841deb042b1e7ec8edf344c3cf14d9b65
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-15 20:18:33 +0400

    refactor: separate app.js from index

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-import dotenv from "dotenv";
-import express from "express";
-import orderRoutes from "./routes/orderRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-
-dotenv.config();
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.json());
-app.get("/", (req, res) => {
-  res.json({ message: "welcome to postgress" });
-});
-app.use("/api/orders", orderRoutes);
-app.use("/api/users", userRoutes);
-
-app.listen(port, () => {
-  console.log("server is running on", port);
-});
+import app from "./app.js";
