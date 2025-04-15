2aec1bde2284caf6db09cc292a27467e8d354bd1
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-15 22:05:05 +0400

    feat: implement update user functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { createUser, getUsers, signIn } from "../controllers/userController.js";
+import {
+  createUser,
+  getUsers,
+  signIn,
+  updateUser,
+} from "../controllers/userController.js";
 import { auth, isAdmin } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.route("/").get(getUsers).post(auth, isAdmin, createUser);
+router.route("/").get(auth, isAdmin, getUsers).post(auth, isAdmin, createUser);
+router.route("/:id").put(auth, isAdmin, updateUser);
+
 router.route("/signIn").post(signIn);
 
 export default router;
