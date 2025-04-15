a2099711d72af48085d88d96e7b44c3d6614aa2b
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-15 21:55:25 +0400

    feat: implement update order route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createOrder, getOrders } from "../controllers/orderController.js";
-import { auth } from "../middlewares/auth.js";
+import {
+  createOrder,
+  getOrders,
+  updateOrder,
+} from "../controllers/orderController.js";
+import { auth, isAdminOrCourier } from "../middlewares/auth.js";
 
 const router = express.Router();
-
-router.route("/").get(auth, getOrders).post(createOrder);
+router.route("/").get(auth, getOrders).post(auth, createOrder);
+router.route("/:id/status").put(auth, isAdminOrCourier, updateOrder);
 
 export default router;
