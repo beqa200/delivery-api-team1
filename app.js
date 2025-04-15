9d869d440c900e2ba2c096f2161b907a2cf0d932
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-15 21:00:32 +0400

    refactor: move dotenv configuration and server listen logic to index.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import express from "express";
-import dotenv from "dotenv";
 import orderRoutes from "./routes/orderRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "welcome to postgress" });
@@ -12,7 +10,4 @@ app.get("/", (req, res) => {
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(port, () => {
-  console.log("server is running on", port);
-});
 export default app;
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+const port = 3000;
+app.listen(port, () => {
+  console.log("server is running on", port);
+});
