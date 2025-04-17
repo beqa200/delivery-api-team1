d9d00c40e65707ef1930d5d2e067729411e0327b
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-17 19:43:07 +0400

    feat: add multer middleware for file uploads

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("server is running on", port);
 });
---
+++ middlewares/uploadFIle.js
@@ -0,0 +1,36 @@
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+const dir = "./uploads";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + path.extname(file.originalname));
+  },
+});
+const filterFiles = (req, file, cb) => {
+  const allowedTypes = [
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.ms-excel",
+  ];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: filterFiles,
+  limits: { fileSize: 5 * 1024 * 1024 },
+}); // 5 MB limit
+
+export default upload;
