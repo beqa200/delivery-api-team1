54b807ae3b9f0de6ce4fba3c2e6e8d1601c4c25f
NikaQanashvili <qanashvilinika7@gmail.com>
2025-04-17 20:04:38 +0400

    feat: add Swagger documentation for Role schema

---
+++ swagger/roles.js
@@ -0,0 +1,16 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Role:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: Unique identifier for the role.
+ *           example: 1
+ *         name:
+ *           type: string
+ *           description: Name of the role.
+ *           example: "Admin"
+ */
